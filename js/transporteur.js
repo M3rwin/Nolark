56a39d4bf933bbc40a36e654845eaaab23e7f891
marco.clin <marco.clin@CP-SIO-W10-PG14.bts.sio>
2023-01-25 17:08:38 +0100

    correction d'erreur dans transporteur.js. réécriture du formulaire html.

--- js/transporteur.js
+++ js/transporteur.js
@@ -1,12 +1,27 @@
+window.addEventListener("load", function () 
+{
+    window.document.querySelector("#bouton").addEventListener("click", validEnvoi);
+}
+);
+
+
+
+
 function getId(id){
-    var valeur = window.document.getElementById(id).value;
+    var valeur = window.document.querySelector(id).value;
     return valeur;
 }
 
-function test(){
-
+function validEnvoi() {
+    if (window.document.querySelector("#km").value === "" || window.document.querySelector("#anciennete").value === "" || (window.document.querySelector("#accident").value === ""))
+    {
+        alert("Tout les champs doivent être rempli"); // On affiche un message
+    } 
+    else 
+    {
+        calculer(); 
+    }
 }
-
 /**
  * Fonction qui renvoie une prime en fonction du nombre de kilomètres parcourus
  * @param {int} km
@@ -28,7 +43,7 @@ function distance(km){
  * @param {int} anne
  * @returns {float}
  */
-function anciennete(anne){
+function getAnciennete(anne){
     const ancienMin=4, primeBase=300, primeSup=30;
     var prime=0.0;
     if(anne>=4){
@@ -51,7 +66,19 @@ function penalite(accidents, prime){
     else if(accidents===3){
         return prime/4;
     }
+    else if(accidents===0){
+        return null;
+    }
     else{
         console.warn("VOUS ÊTES VIRÉ");
     }
+}
+
+
+function calculer(){
+    var kmParcouru = parseInt(getId("#km"));
+    var nbAccident = parseInt(getId("#accident"));
+    var anciennete = parseInt(getId("#anciennete"));
+    var reponse = distance(kmParcouru) + penalite(nbAccident) + getAnciennete(anciennete);
+    alert(reponse);
 }
